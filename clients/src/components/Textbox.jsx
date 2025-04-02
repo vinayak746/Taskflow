c5c75ccb31fc2835001f132ad7884b63c887ebc1
Vinayak Arora <vinayakarora7461@gmail.com>
2025-04-02 19:39:16 +0530

    almost completed the login page

--- clients/src/components/Textbox.jsx
+++ clients/src/components/Textbox.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import clsx from "clsx";
 
 const Textbox = React.forwardRef(
-  ({ type, placeholder, label, className, register, error, name }, ref) => {
+  ({ type, placeholder, label, className, register, name, error }, ref) => {
     return (
       <div className="w-full flex flex-col gap-1">
         {label && (
-          <label htmlFor="{label}" className="text-slate-800">
+          <label htmlFor={name} className="text-slate-800">
             {label}
           </label>
         )}
+
         <div>
           <input
             type={type}
@@ -19,15 +20,16 @@ const Textbox = React.forwardRef(
             {...register}
             aria-invalid={error ? "true" : "false"}
             className={clsx(
-              "bg-transparent px-3 py-2.5 2xl:py-3 border border-gray-300 placeholder:gray-400 text-gray-900 outline-none text-base focus:ring-2  ring-blue-300",
+              "bg-transparent px-3 py-2.5 2xl:py-3 border border-gray-300 placeholder-gray-400 text-gray-900 outline-none text-base focus:ring-2 ring-blue-300",
               className
             )}
           />
         </div>
-        {error && <span className="text-[#f64949fe] text-xs">{error}</span>}
+        {error && (
+          <span className="text-xs text-[#f64949fe] mt-0.5 ">{error}</span>
+        )}
       </div>
     );
   }
 );
-
 export default Textbox;
--- clients/src/pages/Login.jsx
+++ clients/src/pages/Login.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import Textbox from "../components/Textbox.jsx";
+import Button from "../components/Button.jsx";
 const login = () => {
   const user = "";
   const {
@@ -18,15 +19,15 @@ const login = () => {
     user && navigate("/dashboard");
   }, [user]);
   return (
-    <div className="w-full h-screen flex items-cener justify-center flex-col lg:flex-row bg-[#f3f4f6]">
+    <div className="w-full min-h-screen flex items-center justify-center flex-col lg:flex-row bg-[#f3f4f6]">
       <div className="w-full md:w-auto flex gap-0 md:gap-40 flex-col md:flex-row items-center justify-center">
         {/* left side */}
         <div className="h-full w-full lg:w-2/3 flex flex-col items-center justify-center">
-          <div className="w-full md:max-w-lg 2xl:max-w-3xl flex flex-col items-center justify-center gap-5 md:gap-y-10 2xl:mt-20">
-            <span className="flex gap-1 py-1 px-3  border rounded-full text-sm md:text-base border-gray-300 text-gray-600">
-              Manage all your task in one place !
+          <div className="w-full md:max-w-lg 2xl:max-w-3xl flex flex-col items-center justify-center gap-5 md:gap-y-10 2xl:-mt-20">
+            <span className="flex gap-1 py-1 px-3 border rounded-full text-sm md:text-base bordergray-300 text-gray-600">
+              Manage all your task in one place!
             </span>
-            <p className="flex flex-col gap-0 md:gap-5 text-4xl md:text-6xl 2xl:text-7xl font-black text-center text-blue-700">
+            <p className="flex flex-col gap-0 md:gap-4 text-4xl md:text-6xl 2xl:text-7xl font-black text-center text-blue-700">
               <span>Cloud-Based</span>
               <span>Task Manager</span>
             </p>
@@ -37,30 +38,51 @@ const login = () => {
           </div>
         </div>
         {/* right side */}
-        <div className="w-full md:w-1/3 p-5 md:p-1 flex flex-col justify-center items-center">
+        <div className="w-full md:w-1/3 p-4 md:p-1 flex flex-col justify-center items-center">
           <form
             onSubmit={handleSubmit(submitHandler)}
             className="form-container w-full md:w-[400px] flex flex-col gap-y-8 bg-white px-10 pt-14 pb-14"
           >
             <div className="">
-              <p className="text-blue-600 tex-3xl font-bold text-center ">
-                Welcome Back!
+              <p className="text-blue-600 text-3xl font-bold text-center">
+                Welcome back!
               </p>
-              <span className="text-center text-base text-gray-700 flex flex-col ">
-                Keep all your credential safe
-              </span>
+              <p className="text-center text-base text-gray-700 ">
+                Keep all your credential safe.
+              </p>
             </div>
-            <div className="flex flex-col gap-y-5 w-full">
+
+            <div className="flex flex-col gap-y-5">
               <Textbox
                 placeholder="email@example.com"
                 type="email"
                 name="email"
                 label="Email Address"
-                className="w-full rounded-full mb-1.5"
+                className="w-full rounded-full"
                 register={register("email", {
-                  required: "Email Address is required",
+                  required: "Email Address is required!",
                 })}
                 error={errors.email ? errors.email.message : ""}
+              />
+              <Textbox
+                placeholder="your password"
+                type="password"
+                name="password"
+                label="Password"
+                className="w-full rounded-full"
+                register={register("password", {
+                  required: "Password is required!",
+                })}
+                error={errors.password ? errors.password.message : ""}
+              />
+
+              <span className="text-sm text-gray-500 hover:text-blue-600 hover:underline cursor-pointer">
+                Forget Password?
+              </span>
+              <Button
+                type="submit"
+                label="Submit"
+                className="w-full h-10 bg-blue-700 text-white rounded-full hover:bg-blue-800 transition-all duration-200 ease-in-out"
               />
             </div>
           </form>
